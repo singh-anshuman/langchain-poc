ddc3a1b19fcde3772dc67823e6d68e9ea45bdaa4
singh-anshuman <anshumansingh137@gmail.com>
2024-06-01 13:26:44 +0530

    Added python package for scraping data from linkedin

---
+++ third_parties/linkedin.py
@@ -0,0 +1,47 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def scrape_linked_in_profile(linkedin_profile_url: str, mock: bool = False):
+    """scrape information from LinkedIn profiles,
+    Manually scrape the information from the LinkedIn profile"""
+
+    if mock:
+        linkedin_profile_url = "https://gist.githubusercontent.com/singh-anshuman/968bbc71d1cf0fb02872d5401c752e97/raw/800c3452c661b99acf16559f30932b8a95cce732/anshumansingh137_li.json"
+        response = requests.get(
+            linkedin_profile_url,
+            timeout=10,
+        )
+    else:
+        api_endpoint = "https://nubela.co/proxycurl/api/v2/linkedin"
+        header_dic = {"Authorization": f'Bearer {os.environ.get("PROXYCURL_API_KEY")}'}
+        response = requests.get(
+            api_endpoint,
+            params={"url": linkedin_profile_url},
+            headers=header_dic,
+            timeout=10,
+        )
+
+    data = response.json()
+    data = {
+        k: v
+        for k, v in data.items()
+        if v not in ([], "", "", None)
+        and k not in ["people_also_viewed", "certifications"]
+    }
+    if data.get("groups"):
+        for group_dict in data.get("groups"):
+            group_dict.pop("profile_pic_url")
+
+    return data
+
+
+if __name__ == "__main__":
+    print(
+        scrape_linked_in_profile(
+            linkedin_profile_url="https://www.linkedin.com/in/eden-marco/", mock=True
+        )
+    )
---
+++ third_parties/proxycurl_linkedin.py
@@ -0,0 +1,23 @@
+import requests
+
+api_key = 'nx6BO7cY9O1hw_z319WGxw'
+headers = {'Authorization': 'Bearer ' + api_key}
+api_endpoint = 'https://nubela.co/proxycurl/api/v2/linkedin'
+params = {
+    'linkedin_profile_url': 'https://linkedin.com/in/anshumansingh137/',
+    'extra': 'include',
+    'github_profile_id': 'include',
+    'facebook_profile_id': 'include',
+    'twitter_profile_id': 'include',
+    'personal_contact_number': 'include',
+    'personal_email': 'include',
+    'inferred_salary': 'include',
+    'skills': 'include',
+    'use_cache': 'if-present',
+    'fallback_to_cache': 'on-error',
+}
+response = requests.get(api_endpoint,
+                        params=params,
+                        headers=headers)
+
+print(response.content)
